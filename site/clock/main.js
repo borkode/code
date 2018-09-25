c21e3953374cd1229baeaa0abe9ffab967f16288
borkode <31637006+borkode@users.noreply.github.com>
2018-09-25 11:25:23 -0400

    Update main.js

--- site/clock/main.js
+++ site/clock/main.js
@@ -3,13 +3,16 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.beginPath();
 if(m>985){
+ctx.strokeStyle='#fff'
 ctx.Rect(425/2-75,425/2-75,150,150);
+ctx.stroke()
 }
-ctx.fillRect(0,0,425,425,'#666')
+ctx.beginPath();
+ctx.fillRect(0,0,425,425,'#fff')
 ctx.strokeStyle = '#fff'
 ctx.font="25px Arial";
 var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
-gradient.addColorStop("0", "#444");
+gradient.addColorStop("0", "#fff");
 /*ctx.fillStyle = gradient;
 ctx.lineWidth=90;
 ctx.strokeStyle="#444";
@@ -22,42 +25,43 @@ var h = new Date().getHours();
 ctx.beginPath();
 ctx.lineWidth=13
 ctx.strokeStyle='#333'
-ctx.arc(200,200,45,0,2*Math.PI);
-ctx.moveTo(230,200);
-ctx.arc(200,200,30,0,2*Math.PI);
-ctx.moveTo(260,200);
-ctx.arc(200,200,60,0,2*Math.PI);
-ctx.moveTo(275,200);
-ctx.arc(200,200,75,0,2*Math.PI);
-ctx.moveTo(215,200);
-ctx.arc(200,200,15,0,2*Math.PI);
-ctx.moveTo(185,200);
-ctx.arc(200,200,5,0,2*Math.PI);
-ctx.moveTo(290,200);
-ctx.arc(200,200,90,0,2*Math.PI);
-ctx.moveTo(300,200);
-ctx.arc(200,200,100,0,2*Math.PI);
-ctx.moveTo(305,200);
-ctx.arc(200,200,105,0,2*Math.PI);
+var x = 87.5
+ctx.arc(200-x,200-x,45,0,2*Math.PI);
+ctx.moveTo(230-x,200-x);
+ctx.arc(200-x,200-x,30,0,2*Math.PI);
+ctx.moveTo(260-x,200-x);
+ctx.arc(200-x,200-x,60,0,2*Math.PI);
+ctx.moveTo(275-x,200-x);
+ctx.arc(200-x,200-x,75,0,2*Math.PI);
+ctx.moveTo(215-x,200-x);
+ctx.arc(200-x,200-x,15,0,2*Math.PI);
+ctx.moveTo(185-x,200-x);
+ctx.arc(200-x,200-x,5,0,2*Math.PI);
+ctx.moveTo(290-x,200-x);
+ctx.arc(200-x,200-x,90,0,2*Math.PI);
+ctx.moveTo(300-x,200-x);
+ctx.arc(200-x,200-x,100,0,2*Math.PI);
+ctx.moveTo(305-x,200-x);
+ctx.arc(200-x,200-x,105,0,2*Math.PI);
 ctx.stroke();
 ctx.beginPath();
 ctx.strokeStyle='#fff'
 ctx.lineWidth=13;
-ctx.arc(200,200,45,0,d*(2/60*Math.PI));
-ctx.moveTo(230,200);
-ctx.arc(200,200,30,0,m*(2/925*Math.PI));
-ctx.moveTo(260,200);
-ctx.arc(200,200,60,0,n*(2/60*Math.PI));
-ctx.moveTo(275,200);
-ctx.arc(200,200,75,0,h*(2/24*Math.PI));
-ctx.moveTo(215,200);
-ctx.arc(200,200,15,0,2*Math.PI);
-ctx.moveTo(185,200);
-ctx.arc(200,200,5,0,2*Math.PI);
-ctx.moveTo(290,200);
-ctx.arc(200,200,90,0,2*Math.PI);
-ctx.moveTo(300,200);
-ctx.arc(200,200,100,0,2*Math.PI);
+ctx.arc(200-x,200-x,45,0,d*(2/60*Math.PI));
+ctx.moveTo(230-x,200-x);
+ctx.arc(200-x,200-x,30,0,m*(2/925*Math.PI));
+ctx.moveTo(260-x,200-x);
+ctx.arc(200-x,200-x,60,0,n*(2/60*Math.PI));
+ctx.moveTo(275-x,200-x);
+ctx.arc(200-x,200-x,75,0,h*(2/24*Math.PI));
+ctx.moveTo(215-x,200-x);
+ctx.arc(200-x,200-x,15,0,2*Math.PI);
+ctx.moveTo(185-x,200-x);
+ctx.arc(200-x,200-x,5,0,2*Math.PI);
+ctx.moveTo(290-x,200-x);
+ctx.arc(200-x,200-x,90,0,2*Math.PI);
+ctx.moveTo(300-x,200-x);
+ctx.arc(200-x,200-x,100,0,2*Math.PI);
 ctx.stroke();
 
 },1);
