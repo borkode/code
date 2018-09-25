bcf635055078969a03c4a556409bcb6c0cc09fa6
borkode <31637006+borkode@users.noreply.github.com>
2018-09-25 10:24:37 -0400

    Update main.js

--- site/clock/main.js
+++ site/clock/main.js
@@ -5,7 +5,7 @@ ctx.beginPath();
 if(m>985){
 ctx.Rect(425/2-75,425/2-75,150,150);
 }
-ctx.fillRect(0,0,425,425,'#444')
+ctx.fillRect(0,0,425,425,'#666')
 ctx.strokeStyle = '#fff'
 ctx.font="25px Arial";
 var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
@@ -15,10 +15,32 @@ ctx.lineWidth=90;
 ctx.strokeStyle="#444";
 ctx.arc(200,200,45,0,Math.PI*2);
 ctx.stroke();*/
-var d = new Date().getSeconds();
+var d = new Date().getSeconds()+new Date().getMilliseconds()/1000;
 var m = new Date().getMilliseconds();
-var n = new Date().getMinutes();
+var n = new Date().getMinutes()+new Date().getSeconds()/60;
 var h = new Date().getHours();
+ctx.beginPath();
+ctx.lineWidth=13
+ctx.strokeStyle='#333'
+ctx.arc(200,200,45,0,2*Math.PI);
+ctx.moveTo(230,200);
+ctx.arc(200,200,30,0,2*Math.PI);
+ctx.moveTo(260,200);
+ctx.arc(200,200,60,0,2*Math.PI);
+ctx.moveTo(275,200);
+ctx.arc(200,200,75,0,2*Math.PI);
+ctx.moveTo(215,200);
+ctx.arc(200,200,15,0,2*Math.PI);
+ctx.moveTo(185,200);
+ctx.arc(200,200,5,0,2*Math.PI);
+ctx.moveTo(290,200);
+ctx.arc(200,200,90,0,2*Math.PI);
+ctx.moveTo(300,200);
+ctx.arc(200,200,100,0,2*Math.PI);
+ctx.moveTo(305,200);
+ctx.arc(200,200,105,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
 ctx.strokeStyle='#fff'
 ctx.lineWidth=13;
 ctx.arc(200,200,45,0,d*(2/60*Math.PI));
@@ -28,11 +50,14 @@ ctx.moveTo(260,200);
 ctx.arc(200,200,60,0,n*(2/60*Math.PI));
 ctx.moveTo(275,200);
 ctx.arc(200,200,75,0,h*(2/24*Math.PI));
-ctx.moveTo(210,200);
+ctx.moveTo(215,200);
 ctx.arc(200,200,15,0,2*Math.PI);
-ctx.moveTo(180,200);
+ctx.moveTo(185,200);
 ctx.arc(200,200,5,0,2*Math.PI);
-ctx.moveTo(295,200);
+ctx.moveTo(290,200);
 ctx.arc(200,200,90,0,2*Math.PI);
+ctx.moveTo(300,200);
+ctx.arc(200,200,100,0,2*Math.PI);
 ctx.stroke();
+
 },1);
